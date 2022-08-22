84c18de1abec720e71fbe575f81b0f4a140b82d3
lucy1528 <luxysm@gmail.com>
2022-08-22 18:51:20 -0500

    ejercicio 1, open bootcamp

---
+++ ejercicio1.js
@@ -0,0 +1,10 @@
+let nombre = "Lucia Cerpa";
+let edad = 37;
+let eresDesarrollador = true;
+let fechaDeNacimiento = new Date('June 28, 1985');
+let libroFavorito = {
+    titulo:"Francesco",
+    autor:"Garcia, Yohana",
+    fecha:"2011",
+    url:"https://www.elsotano.com/libro/francesco-una-vida-entre-el-cielo-y-la-tierra_10373227"
+}
